c5b1e03cce588194e98163d355451bec4a330f99
bwapes <benwaples@gmail.com>
2020-08-05 16:38:04 -0700

    pokeball is showing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import request from 'superagent';
 import Header from './Header.js'
 import Search from './Search.js'
 import PokemonList from './PokemonList.js'
+import styles from './App.css'
 import './App.css';
 
 
@@ -10,14 +11,16 @@ export default class App extends Component {
   state ={
     filteredPokemon: [],
     searchKeyword: '',
-    categories: []
+    isLoading: false,
   }
 
   handleSubmit = async () => {
+    this.setState({isLoading: true})
     const data = await request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex?perPage=400&pokemon=${this.state.searchKeyword}`)
 
     this.setState({
-      filteredPokemon: data.body.results
+      filteredPokemon: data.body.results,
+      isLoading: false
     })
 
     console.log(this.state.filteredPokemon)
@@ -36,7 +39,9 @@ export default class App extends Component {
             <Search handleSearch={this.handleSearch} />
             <button onClick={this.handleSubmit}>Find Pokemon</button>
           </div>
+            {this.state.isLoading ? <img className={styles.spin} src="pokeball.png" alt="pokeball"/> :
             <PokemonList displayPoke={this.state.filteredPokemon}/>
+            }
         </div>
       </main>
     )

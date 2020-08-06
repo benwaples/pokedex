7ba3dc8d2c6f8da0cf795eb1a95ab6e7f8c78cb4
bwapes <benwaples@gmail.com>
2020-08-06 14:11:39 -0700

    filter is working and in its own component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import request from 'superagent';
 import Header from './Header.js'
 import Search from './Search.js'
+import Option from './Option.js'
 import PokemonList from './PokemonList.js'
 import styles from './App.css'
 import './App.css';
@@ -10,13 +11,13 @@ import './App.css';
 export default class App extends Component {
   state ={
     filteredPokemon: [],
-    searchKeyword: '',
+    searchFilter: 'pokemon',
     isLoading: false,
   }
 
   handleSubmit = async () => {
     this.setState({isLoading: true})
-    const data = await request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex?perPage=400&pokemon=${this.state.searchKeyword}`)
+    const data = await request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex?perPage=800&${this.state.searchFilter}=${this.state.searchKeyword}`)
 
     this.setState({
       filteredPokemon: data.body.results,
@@ -24,6 +25,8 @@ export default class App extends Component {
     })
 
     console.log(this.state.filteredPokemon)
+    console.log(this.state.searchFilter)
+
   }
 
   handleSearch = (e) => {
@@ -37,6 +40,9 @@ export default class App extends Component {
         <div id="row">
           <div id="left">
             <Search handleSearch={this.handleSearch} />
+            <select onChange={(e) => {this.setState({searchFilter: e.target.value})}}>
+                <Option />
+            </select> 
             <button onClick={this.handleSubmit}>Find Pokemon</button>
           </div>
             {this.state.isLoading ? <img className={styles.spin} src="pokeball.png" alt="pokeball"/> :
